4760ed65c16155bd0adce3137ed4d47a40d4c391
JakobLarsNaurHansen <s224312@student.dtu.dk>
2022-09-25 12:45:35 +0200

    added TestDice folder

--- src/Test/Test.java
+++ src/Test/Test.java
@@ -1,4 +1,15 @@
 package Test;
+import java.lang.Math;
 
 public class Test {
+
+    public static void dice(String[] args){
+
+
+        int dice1= (int)(Math.random()*6+1);
+        int dice2= (int)(Math.random()*6+1);
+
+        int sum= dice1 + dice2;
+        System.out.println(sum);
+    }
 }

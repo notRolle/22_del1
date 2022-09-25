fcbc010e735f2fdf344bb21e9d7f958a96f33015
JakobLarsNaurHansen <s224312@student.dtu.dk>
2022-09-25 14:04:21 +0200

    Tilføj kommentar og rettede ind på testen.

--- src/Test/Test.java
+++ src/Test/Test.java
@@ -3,13 +3,7 @@ import java.lang.Math;
 
 public class Test {
 
-    public static void dice(String[] args){
+    public static void main(String[] args){
 
-
-        int dice1= (int)(Math.random()*6+1);
-        int dice2= (int)(Math.random()*6+1);
-
-        int sum= dice1 + dice2;
-        System.out.println(sum);
     }
 }
--- src/Test/TestDice.java
+++ src/Test/TestDice.java
@@ -1,27 +1,29 @@
 package Test;
 
 public class TestDice {
+
+    //Test Terning
+    public static int dice(){
+        int dice1 = (int) (Math.random() * 6 + 1);
+        int dice2 = (int) (Math.random() * 6 + 1);
+        //Beregner og printer summen af øjne
+        return dice1 + dice2;
+    }
     public static void main(String[] args) {
 
         //-------------------------Tester normalfordeling--------------------------------
 
-        //tomt array, men antallet af værdier som summen af to terninger kan give.
+        //Tomt array, med samme antal værdier som summen af to terninger kan give.
         int[] num = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
+        //Loop for 1000 kast
         for (int j = 0; j <1000 ; j++) {
-            // To terninger
-            int dice1 = (int) (Math.random() * 6 + 1);
-            int dice2 = (int) (Math.random() * 6 + 1);
-            //Beregner og printer summen af øjne
-            int sum = dice1 + dice2;
-
-            num[sum-2]+=1;
+            // Notere hvad der blev slået for hvert kast i num array.
+            num[dice()-2]+=1;
         }
+        //printer antaller at hvert slag.
         for (int i = 0; i < num.length; i++) {
-            System.out.println(num[i]);
+            System.out.println((i+2)+": " + num[i]);
         }
-
-
-
     }
 }

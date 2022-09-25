806016873d473deb24dffeb621d19addbf1cc8e6
JakobLarsNaurHansen <s224312@student.dtu.dk>
2022-09-25 16:15:25 +0200

    Tilføjet noter og stops for at forbedre spille oplevelsen

--- src/Spil/Main.java
+++ src/Spil/Main.java
@@ -1,7 +1,5 @@
 package Spil;
 
-import Test.TestDice;
-
 import java.util.Scanner;
 
 import static Test.TestDice.*;
@@ -11,53 +9,89 @@ public class Main {
         //Initialisere spiller 1 og 2. sum=antal points, roll=værdi af kast.
         int player1roll=0, player1sum=0, player2roll=0, player2sum=0;
 
+        // Looper skiftevis kast, så begge spillere når at kaste lige mange gange
         while (player1sum<40 || player2sum<40){
             Scanner input = new Scanner(System.in);
 
+            //Spiller 1 tur
             System.out.println("________________________________________");
             System.out.println("Player 1! Hit ENTER to roll! ");
-            String pause1 = input.nextLine();
-            player1roll=dice();
-            player1sum += player1roll;
-            System.out.println("You rolled "+player1roll+" and have "+player1sum + "points!");
-            player1roll = 0;
+            String pause1 = input.nextLine(); //Indsat pause, så spiller 1 skal gøre noget for at kaste
+            player1roll=dice(); //Terning kast
+            player1sum += player1roll; // Tilføjer kast til sum
+            System.out.println("You rolled "+player1roll+" and have "+player1sum + "points!"); //Printer kast og points
 
+            //Spiller 2 tur
             System.out.println("________________________________________");
             System.out.println("Player 2! Hit ENTER to roll! ");
             String pause2 = input.nextLine();
             player2roll = dice();
             player2sum += player2roll;
             System.out.println("You rolled "+player2roll+" and have "+player2sum + "points!");
-            player2roll = 0;
 
+
+            //Checker om spiller 1 har vundet
             if (player1sum >=40 && player2sum < 40) {
                 System.out.println("Player 1 wins!!!");
                 break;
             }
+            //Checker om spiller 2 har vundet
             if (player2sum >=40 && player1sum < 40) {
                 System.out.println("Player 2 wins!!!");
                 break;
             }
+            //Checker om begge spillere har over 40 points
             if (player2sum >=40 && player1sum >= 40) {
+                //Begge spillere kaster igen
                 player1roll = dice();
                 player2roll = dice();
+
+                //Spiller 1 kaster igen
+                System.out.println();
+                System.out.println("Both players have over 40 points! ");
+                System.out.println("Player 1! Hit ENTER to roll");
+                String pause3 = input.nextLine();
+                System.out.println("Player 1 roll "+ player1roll);
+                System.out.println("");
+
+                //Spiller 2 kaster igen
+                System.out.println("Player 2! Hit ENTER to roll");
+                String pause4 = input.nextLine();
+                System.out.println("Player 2 roll "+ player2roll);
+                System.out.println("");
+
+                //Checker hvem det slog det højeste slag og vinder
                 if (player1roll > player2roll){
-                    System.out.println("Player 1 rolls " + player1roll + "! And Player 2 rolls " + player2roll + "!");
                     System.out.println("Player 1 wins!!!");
                 }
                 if (player1roll < player2roll){
-                    System.out.println("Player 1 rolls " + player1roll + "! And Player 2 rolls " + player2roll + "!");
                     System.out.println("Player 2 wins!!!");
                 }
+
+                //Hvis de slog det samme looper den indtil en vinder.
                 while (player1roll==player2roll){
+
+                    //Begge spillere kaster igen
                     player1roll = dice();
                     player2roll = dice();
+
+                    //Spiller 1 kaster igen
+                    System.out.println("the same? Roll again player 1! Hit ENTER");
+                    String pause5 = input.nextLine();
+                    System.out.println("Player 1 roll "+ player1roll);
+                    System.out.println();
+
+                    //Spiller 2 kaster igen
+                    System.out.println("Player 2! Hit ENTER to roll");
+                    String pause6 = input.nextLine();
+                    System.out.println("Player 2 roll "+ player2roll);
+                    System.out.println();
+
+                    //Checker hvem der har vundet
                     if (player1roll > player2roll){
-                        System.out.println("Player 1 rolls " + player1roll + "! And Player 2 rolls " + player2roll + "!");
                         System.out.println("Player 1 wins!!!");
                     }
                     if (player1roll < player2roll){
-                        System.out.println("Player 1 rolls " + player1roll + "! And Player 2 rolls " + player2roll + "!");
                         System.out.println("Player 2 wins!!!");
                     }
                 }
